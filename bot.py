ec56828238689d3efa47dc3d482a7c3ff651e1a3
jashanbeniwal0101 <beniwaljass733@gmail.com>
2025-06-08 19:23:55 +0530

    Added asyncio for Bot.run()

--- bot.py
+++ bot.py
@@ -1,15 +1,18 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
+import logging
+import asyncio
 from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
+
+
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
+
 
 class Bot(Client):
 
     def __init__(self):
         super().__init__(
-            "techvj login",
+            "Save Rest.. bot",
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
@@ -22,15 +25,20 @@ class Bot(Client):
     async def start(self):
             
         await super().start()
-        print('Bot Started Powered By @VJ_Botz')
+        LOGGER(__name__).info('Bot Started Powered By @VJ_Botz')
 
     async def stop(self, *args):
 
         await super().stop()
-        print('Bot Stopped Bye')
+        LOGGER(__name__).info('Bot Stopped Bye')
 
-Bot().run()
+if __name__ == '__main__':
+    bot = Bot()
+    try:
+        asyncio.run(bot.run())
+    except Exception as e:
+        LOGGER(__name__).info(f'Error Starting: {e}')
+        LOGGER(__name__).info('Starting Again')
+        asyncio.run(bot.run())
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+
